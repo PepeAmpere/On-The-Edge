7a1404b72c8892135c6a7959f90435476466c823
PepeAmpere <machys@gmail.com>
2013-12-21 14:50:20 +0100

    FIX
    - hotfix of changed model animation script

--- scripts/minebasic.lua
+++ scripts/minebasic.lua
@@ -1,28 +1,18 @@
-local base = piece "base"
-local body = piece "body"
-local turret = piece "turret"
-local gun = piece "gun"
-local flare = piece "flare"
--- declares all the pieces we'll use in the script.
-
 local SIG_AIM = 2
-
-local RESTORE_DELAY = Spring.UnitScript.GetLongestReloadTime(unitID) * 2
--- picks a sensible time to wait before trying to turn the turret back to default.
 
 function script.Create()
     return 0
 end
 
 function script.AimWeapon(weaponID, heading, pitch)
-	return true
+	-- return true
 end
 
 function script.FireWeapon(weaponID)
-	EmitSfx(flare, 0)
+	-- EmitSfx(flare, 0)
 end
 
-function script.QueryWeapon() return flare end
+-- function script.QueryWeapon() return flare end
 -- The piece that the bullet/laser/whatever comes out of.
 
 function script.AimFromWeapon() return gun end
@@ -34,7 +24,7 @@ end
 
 function script.HitByWeapon(x,z,weaponDef,damage)
     -- This stops the unit taking damage until it's been built.
-	if GetUnitValue(COB.BUILD_PERCENT_LEFT)>2 then return 0
-	else return damage
-	end
+	-- if GetUnitValue(COB.BUILD_PERCENT_LEFT)>2 then return 0
+	-- else return damage
+	-- end
 end
