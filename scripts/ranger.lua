e85983bd89985d5c76ef1a61ecedc06a9596cb1e
bageta <owde@seznam.cz>
2014-01-16 18:11:11 +0100

    soldier and ranger explodes when destroyed and soldier aims and shoot animantions added to script

--- scripts/ranger.lua
+++ scripts/ranger.lua
@@ -1,18 +1,19 @@
 local butt				= piece 'butt'
 local left_leg_top		= piece 'left_leg_top'
 local left_leg_middle	= piece 'left_leg_middle'
-local left_leg_feet		= piece 'left_leg_feet'
+local left_leg_feet		= piece 'left_lef_feet'
 local right_leg_top		= piece 'right_leg_top'
 local right_leg_middle	= piece 'right_leg_middle'
 local right_leg_feet	= piece 'right_leg_feet'
 local body				= piece 'body'
-local container			= piece 'cantainer' 
+local container			= piece 'container' 
 local left_top_tube		= piece 'left_top_tube'
 local right_top_tube	= piece 'right_top_tube'
 local left_gun			= piece 'left_bottom_tube'
 local right_gun			= piece 'right_bottom_tube'
 
 local moving = false
+local gunSelect = false		
 
 local SIG_Aim	= 2
 local SIG_Move	= 1
@@ -25,8 +26,8 @@ end
 local function Walk()
 	Signal(SIG_Walk)
 	SetSignalMask(SIG_Walk)
-	while (true) do
-	end
+	-- while (true) do
+	-- end
 end
 
 local function StopWalk()
@@ -45,14 +46,62 @@ function script.StopMoving()
 end
 
 function script.AimWeapon1()
+	Signal(SIG_Aim)
+	SetSignalMask(SIG_Aim)
 end
 
 function script.FireWeapon1()
 end
 
 function script.AimFromWeapon1()
+	return body
 end
 
 function script.Killed(recentDamage, maxHealth)
-	return 0
+	local severity = recentDamage/maxHealth*100
+	if( severity <= 25 ) then
+		Explode( left_top_tube, SFX.SHATTER)
+		Explode( right_top_tube, SFX.SHATTER)
+		Explode( body, SFX.SHATTER)
+		Explode( left_leg_middle, SFX.FALL)
+		Explode( left_leg_feet, SFX.FALL)
+		Explode( right_leg_middle, SFX.FALL)
+		return (1)
+	end
+	if( severity <= 50 ) then
+		Explode( left_top_tube, SFX.SHATTER)
+		Explode( right_top_tube, SFX.SHATTER)
+		Explode( body, SFX.SHATTER)
+		Explode( right_leg_top, SFX.FALL)
+		Explode( right_leg_middle, SFX.FALL)
+		Explode( right_leg_feet, SFX.FALL)
+		Explode( left_leg_top, SFX.FALL)
+		Explode( left_leg_middle, SFX.FALL)
+		Explode( left_leg_feet, SFX.FALL)
+		return (2)
+	end
+	if( severity <= 99 ) then
+		Explode( left_top_tube, SFX.SHATTER)
+		Explode( right_top_tube, SFX.SHATTER)
+		Explode( body, SFX.SHATTER)
+		Explode( right_leg_top, SFX.FALL)
+		Explode( right_leg_middle, SFX.FALL)
+		Explode( right_leg_feet, SFX.SHATTER)
+		Explode( left_leg_top, SFX.FALL)
+		Explode( left_leg_middle, SFX.FALL)
+		Explode( left_leg_feet, SFX.FALL)
+		return (3)
+	end
+	Explode( left_top_tube, SFX.FALL)
+	Explode( right_top_tube, SFX.FALL)
+	Explode( left_top_tube, SFX.FIRE)
+	Explode( right_top_tube, SFX.FIRE)
+	Explode( body, SFX.SHATTER)
+	Explode( right_leg_top, SFX.SHATTER)
+	Explode( right_leg_middle, SFX.FALL)
+	Explode( right_leg_feet, SFX.FALL)
+	Explode( left_leg_top, SFX.FALL)
+	Explode( left_leg_middle, SFX.FALL)
+	Explode( left_leg_feet, SFX.FALL)
+	return (3)
 end
--- scripts/soldier.lua
+++ scripts/soldier.lua
@@ -11,7 +11,8 @@ local right_shield		= piece 'right_shield'
 local left_gun			= piece 'left_gun'
 local right_gun			= piece 'right_gun'
 
-local moving = false
+local moving 	= false
+local gunSelect	= false
 
 local SIG_Aim	= 2
 local SIG_Move	= 1
@@ -24,13 +25,22 @@ end
 local function Walk()
 	Signal(SIG_Walk)
 	SetSignalMask(SIG_Walk)
-	while (true) do
-	end
+	-- while (true) do
+	-- end
 end
 
 local function StopWalk()
 	Signal(SIG_Walk)
 	SetSignalMask(SIG_Walk)
+end
+
+local function RestoreAfterDelay()
+	Sleep(1000)
+	
+	Turn(body, y_axis, math.rad(0), math.rad(90))
+	WaitForTurn(body, y_axis)
+	Turn(left_gun, x_axis, math.rad(0), math.rad(90))
+	Turn(right_gun, x_axis, math.rad(0), math.rad(90))
 end
 
 function script.StartMoving()
@@ -43,15 +53,87 @@ function script.StopMoving()
 	StartThread( StopWalk )
 end
 
-function script.AimWeapon1()
+function script.AimWeapon1(heading, pitch)
+	Signal(SIG_Aim)
+	SetSignalMask(SIG_Aim)
+	
+	local newPitch = -pitch 
+	if (pitch <= 0) then
+		newPitch = newPitch * 1.5
+	end
+	
+	Turn(body, y_axis, heading, math.rad(180)) 			-- left-right
+	Turn(left_gun, x_axis, newPitch, math.rad(300)) 	-- up-down left arm
+	Turn(right_gun, x_axis, newPitch, math.rad(300)) -- up-down right arm
+	WaitForTurn(body, y_axis)
+	WaitForTurn(left_gun, x_axis)
+	WaitForTurn(right_gun, x_axis)
+	
+	StartThread( RestoreAfterDelay )
+	return true
+end
+
+function script.QueryWeapon()
+	if gunSelect then 
+		return left_gun
+	else 
+		return right_gun 
+	end
 end
 
 function script.FireWeapon1()
+	gunSelect = not gunSelect
 end
 
 function script.AimFromWeapon1()
+	return body
 end
 
 function script.Killed(recentDamage, maxHealth)
-	return 0
+	local severity = recentDamage/maxHealth*100
+	if( severity <= 25 ) then
+		Explode( left_shield, SFX.SHATTER)
+		Explode( right_shield, SFX.SHATTER)
+		Explode( body, SFX.SHATTER)
+		Explode( left_leg_middle, SFX.FALL)
+		Explode( left_leg_feet, SFX.FALL)
+		Explode( right_leg_middle, SFX.FALL)
+		return (1)
+	end
+	if( severity <= 50 ) then
+		Explode( left_shield, SFX.SHATTER)
+		Explode( right_shield, SFX.SHATTER)
+		Explode( body, SFX.SHATTER)
+		Explode( right_leg_top, SFX.FALL)
+		Explode( right_leg_middle, SFX.FALL)
+		Explode( right_leg_feet, SFX.FALL)
+		Explode( left_leg_top, SFX.FALL)
+		Explode( left_leg_middle, SFX.FALL)
+		Explode( left_leg_feet, SFX.FALL)
+		return (2)
+	end
+	if( severity <= 99 ) then
+		Explode( left_shield, SFX.SHATTER)
+		Explode( right_shield, SFX.SHATTER)
+		Explode( body, SFX.SHATTER)
+		Explode( right_leg_top, SFX.FALL)
+		Explode( right_leg_middle, SFX.FALL)
+		Explode( right_leg_feet, SFX.SHATTER)
+		Explode( left_leg_top, SFX.FALL)
+		Explode( left_leg_middle, SFX.FALL)
+		Explode( left_leg_feet, SFX.FALL)
+		return (3)
+	end
+	Explode( left_shield, SFX.FALL)
+	Explode( right_shield, SFX.FALL)
+	Explode( left_gun, SFX.FIRE)
+	Explode( right_gun, SFX.FIRE)
+	Explode( body, SFX.SHATTER)
+	Explode( right_leg_top, SFX.SHATTER)
+	Explode( right_leg_middle, SFX.FALL)
+	Explode( right_leg_feet, SFX.FALL)
+	Explode( left_leg_top, SFX.FALL)
+	Explode( left_leg_middle, SFX.FALL)
+	Explode( left_leg_feet, SFX.FALL)
+	return (3)
 end
